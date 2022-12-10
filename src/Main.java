2c5d7d84e49e47201817d9bab7ac47edaa5f196c
Oleona <Ole-onna@mail.ru>
2022-12-11 00:38:25 +0300

    Added fix

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
         System.out.print("Input a pair of Roman or Arabic operands and an operation sign in the format: " +
                 "\noperand1 space getOperationSign space operand2:\n ");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine().toUpperCase();
 
-        ParsingResult parsedInput = InputParser.parse(input);
-        ConverterResult preparedData = NumbersConverter.prepareDataForCalculation(parsedInput);
+        var calculationResult = calc(input);
+
+        System.out.println(calculationResult);
+    }
+
+    public static String calc(String input) throws Exception {
+        var parsedInput = InputParser.parse(input);
+        var preparedData = NumbersConverter.prepareDataForCalculation(parsedInput);
 
         Calculator calculator = new Calculator();
         var operationType = calculator.chooseOperationType(preparedData);
         var operationResult = operationType.processOperation();
 
+        String result;
         if (preparedData.getIsRoman()) {
-            System.out.println(NumbersConverter.convertArabicToRoman(operationResult));
+            result = NumbersConverter.convertArabicToRoman(operationResult);
         } else {
-            System.out.println(operationResult);
+            result = String.valueOf(operationResult);
         }
 
+        return result;
     }
 }
